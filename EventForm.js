58405914cae8bda0965284d764094c682e168f9b
Andy Rashev <46199572+andyyrv2020@users.noreply.github.com>
2022-04-19 18:27:53 +0200

    Create EventForm.js

---
+++ EventForm.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {
+    View,
+    Button
+} from 'react-native';  
+
+class EventForm extends Component {
+    render() {
+        return (
+            <View>
+                <Button
+                    title = "Add"
+                />
+            </View>
+        );
+    }
+}
+
+export default EventForm;
